2c1504a9d2bc8f11b5c860428cea047304c8d7e8
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-05-11 20:28:27 +0700

    feat(customer): add health check path and add infrastructure layer to api

--- src/Services/Customer/Customer.Api/Program.cs
+++ src/Services/Customer/Customer.Api/Program.cs
@@ -1,13 +1,21 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Customer.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddInfrastructureService(builder.Configuration);
+
 var app = builder.Build();
+
+app.MapHealthChecks("/health");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
