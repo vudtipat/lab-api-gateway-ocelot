04b5f4170cc1e15f6660db8ca0c59a12d1722d14
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-05-18 23:45:29 +0700

    feat(apigw): add ocelot apigw

---
+++ src/OcelotApiGW/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using MMLib.Ocelot.Provider.AppConfiguration;
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true);
+
+builder.Configuration.AddOcelotWithSwaggerSupport(options =>
+{
+    options.Folder = "Configurations";
+});
+
+builder.Services
+    .AddOcelot(builder.Configuration)
+    .AddAppConfiguration();
+
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.MapControllers();
+app.UseRouting();
+app.MapHealthChecks("/probe");
+
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+});
+
+app.UseOcelot().Wait();
+
+app.Run();
